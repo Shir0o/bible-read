bef88465b74f93708a77b5677d08e1b865008650
Tony Wang <Shir0o@users.noreply.github.com>
2025-07-08 10:52:20 -0700

    Add unit tests for MainPage

--- test/main_page_test.dart
+++ test/main_page_test.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/src/pigeon/mocks.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'package:bible_read/pages/main_page.dart';
-import 'package:bible_read/pages/home_page.dart';
 import 'package:bible_read/pages/read_log_page.dart';
 
 class FakeGoogleSignInPlatform extends GoogleSignInPlatform
@@ -15,55 +13,57 @@ class FakeGoogleSignInPlatform extends GoogleSignInPlatform
   GoogleSignInUserData? user;
   int silentSignInCount = 0;
 
-    @override
-    Future<void> init({
-      List<String> scopes = const <String>[],
-      SignInOption signInOption = SignInOption.standard,
-      String? hostedDomain,
-      String? clientId,
-    }) async {}
+  @override
+  Future<void> init({
+    List<String> scopes = const <String>[],
+    SignInOption signInOption = SignInOption.standard,
+    String? hostedDomain,
+    String? clientId,
+  }) async {}
 
-    @override
-    Future<GoogleSignInUserData?> signInSilently() async {
-      silentSignInCount++;
-      return user;
-    }
-
-    @override
-    Future<GoogleSignInUserData?> signIn() async => user;
-
-    @override
-    Future<GoogleSignInTokenData> getTokens({
-      required String email,
-      bool? shouldRecoverAuth,
-    }) async {
-      return GoogleSignInTokenData(
-        idToken: 'idToken',
-        accessToken: 'accessToken',
-      );
-    }
-
-    @override
-    Future<void> signOut() async {}
-
-    @override
-    Future<void> disconnect() async {}
-
-    @override
-    Future<bool> isSignedIn() async => user != null;
-
-    @override
-    Future<void> clearAuthCache({required String token}) async {}
-
-    @override
-    Future<bool> requestScopes(List<String> scopes) async => true;
-
-    @override
-    Future<bool> canAccessScopes(List<String> scopes, {String? accessToken}) async => true;
-
-    @override
-    Stream<GoogleSignInUserData?>? get userDataEvents => null;
+  @override
+  Future<GoogleSignInUserData?> signInSilently() async {
+    silentSignInCount++;
+    return user;
   }
+
+  @override
+  Future<GoogleSignInUserData?> signIn() async => user;
+
+  @override
+  Future<GoogleSignInTokenData> getTokens({
+    required String email,
+    bool? shouldRecoverAuth,
+  }) async {
+    return GoogleSignInTokenData(
+      idToken: 'idToken',
+      accessToken: 'accessToken',
+    );
+  }
+
+  @override
+  Future<void> signOut() async {}
+
+  @override
+  Future<void> disconnect() async {}
+
+  @override
+  Future<bool> isSignedIn() async => user != null;
+
+  @override
+  Future<void> clearAuthCache({required String token}) async {}
+
+  @override
+  Future<bool> requestScopes(List<String> scopes) async => true;
+
+  @override
+  Future<bool> canAccessScopes(List<String> scopes,
+          {String? accessToken}) async =>
+      true;
+
+  @override
+  Stream<GoogleSignInUserData?>? get userDataEvents => null;
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -95,31 +95,28 @@ void main() {
   testWidgets('navigation updates selected index', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: MainPage()));
     await tester.tap(find.text('Feed'));
-    await tester.pumpAndSettle();
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
     expect(find.byType(ReadLogPage), findsOneWidget);
   });
 
-  testWidgets('attemptSilentSignIn updates user when account exists',
-      (tester) async {
+  testWidgets('attemptSilentSignIn runs during initState', (tester) async {
     fakePlatform.user = GoogleSignInUserData(
       email: 'test@example.com',
       id: '123',
       displayName: 'Test',
     );
     await tester.pumpWidget(const MaterialApp(home: MainPage()));
-    final state = tester.state(find.byType(MainPage)) as dynamic;
-    await state._attemptSilentSignIn();
     await tester.pump();
-    expect(state._user, isNotNull);
     expect(fakePlatform.silentSignInCount, 1);
   });
 
   testWidgets('responsive scaffold switches layout', (tester) async {
     await tester.pumpWidget(
-      MaterialApp(
+      const MaterialApp(
         home: MediaQuery(
-          data: const MediaQueryData(size: Size(800, 600)),
-          child: const MainPage(),
+          data: MediaQueryData(size: Size(800, 600)),
+          child: MainPage(),
         ),
       ),
     );
@@ -127,10 +124,10 @@ void main() {
     expect(find.byType(NavigationBar), findsNothing);
 
     await tester.pumpWidget(
-      MaterialApp(
+      const MaterialApp(
         home: MediaQuery(
-          data: const MediaQueryData(size: Size(400, 600)),
-          child: const MainPage(),
+          data: MediaQueryData(size: Size(400, 600)),
+          child: MainPage(),
         ),
       ),
     );
