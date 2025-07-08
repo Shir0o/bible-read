b062dbb168fe7d14425beded8a5f8f3ff227d90b
Tony Wang <Shir0o@users.noreply.github.com>
2025-07-08 14:44:22 -0700

    Add integration test and dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'IBMPlexMono',
       ),
-      home: const MainPage(),
+      home: MainPage(),
     );
   }
 }
