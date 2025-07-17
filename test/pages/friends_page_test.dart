398c1d7e1900398ae9ed9ebd2e800e8fb5c37a10
Tony Wang <Shir0o@users.noreply.github.com>
2025-07-17 12:10:13 -0700

    Use try/finally for loading states and add regression tests

--- test/pages/friends_page_test.dart
+++ test/pages/friends_page_test.dart
@@ -2,6 +2,7 @@ import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:bible_read/pages/friends_page.dart';
 import 'package:bible_read/services/friend_service.dart';
@@ -19,6 +20,20 @@ class RecordingFriendService extends FriendService {
     required String toEmail,
   }) async {
     lastEmail = toEmail;
+  }
+}
+
+class FailingFriendService extends FriendService {
+  FailingFriendService({required FakeFirebaseFirestore firestore})
+      : super(firestore: firestore);
+
+  @override
+  Future<void> sendFriendRequestByEmail({
+    required String fromUid,
+    required String fromName,
+    required String toEmail,
+  }) async {
+    throw FirebaseException(plugin: 'firestore');
   }
 }
 
@@ -70,5 +85,25 @@ void main() {
     await pumpPage(tester);
 
     expect(find.text('Alice'), findsOneWidget);
+  });
+
+  testWidgets('failed request leaves send button enabled', (tester) async {
+    final failing = FailingFriendService(firestore: firestore);
+    await tester.pumpWidget(
+      MaterialApp(home: FriendsPage(friendService: failing, auth: auth)),
+    );
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+    await tester.enterText(find.byType(TextField), 'x@example.com');
+    await tester.tap(find.text('Send'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    final button = tester.widget<ElevatedButton>(find.widgetWithText(ElevatedButton, 'Send'));
+    expect(button.onPressed, isNotNull);
   });
 }
