5e9ea6880b517f535f07a5df57718c3c39e0eb5e
Tony Wang <Shir0o@users.noreply.github.com>
2025-07-29 09:07:28 -0700

    Handle Crashlytics in tests

--- lib/services/error_logger.dart
+++ lib/services/error_logger.dart
@@ -5,13 +5,16 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 class ErrorLogger {
   ErrorLogger._();
 
-  static final FirebaseCrashlytics _crashlytics = FirebaseCrashlytics.instance;
-
   /// Records [error] and optional [stack] to Crashlytics.
   static Future<void> log(Object error, [StackTrace? stack]) async {
     if (kDebugMode) {
       debugPrint('$error\n$stack');
     }
-    await _crashlytics.recordError(error, stack, fatal: false);
+    try {
+      await FirebaseCrashlytics.instance
+          .recordError(error, stack, fatal: false);
+    } catch (_) {
+      // ignore errors if Crashlytics is unavailable
+    }
   }
 }
