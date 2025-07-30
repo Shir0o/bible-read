b4bf059482957b4fc2ef55655356bf5311bd213c
Tony Wang <Shir0o@users.noreply.github.com>
2025-07-30 11:05:12 -0700

    fix: remove duplicate comment block

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,6 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-/****
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const { onCall } = require("firebase-functions/v2/https");
 const { setGlobalOptions } = require("firebase-functions/v2");
 const functions = require("firebase-functions/v1");
