5370cae3d4e70a14f3a2a269ed63e359c5bc45da
Tony Wang <Shir0o@users.noreply.github.com>
2025-08-07 12:13:50 -0700

    Add model serialization tests

---
+++ test/models/group_test.dart
@@ -0,0 +1,40 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:bible_read/models/group.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Group', () {
+    test('fromFirestore parses data', () async {
+      final firestore = FakeFirebaseFirestore();
+      await firestore.collection('groups').doc('g1').set({
+        'name': 'Test',
+        'ownerUid': 'u1',
+      });
+      final doc = await firestore.collection('groups').doc('g1').get();
+
+      final group = Group.fromFirestore(doc);
+      expect(group.id, 'g1');
+      expect(group.name, 'Test');
+      expect(group.ownerUid, 'u1');
+    });
+
+    test('fromFirestore handles missing fields', () async {
+      final firestore = FakeFirebaseFirestore();
+      await firestore.collection('groups').doc('g1').set({});
+      final doc = await firestore.collection('groups').doc('g1').get();
+
+      final group = Group.fromFirestore(doc);
+      expect(group.name, '');
+      expect(group.ownerUid, '');
+    });
+
+    test('toFirestore outputs expected map', () {
+      const group = Group(id: 'g1', name: 'Test', ownerUid: 'u1');
+      final map = group.toFirestore();
+      expect(map, {'name': 'Test', 'ownerUid': 'u1'});
+    });
+  });
+}
