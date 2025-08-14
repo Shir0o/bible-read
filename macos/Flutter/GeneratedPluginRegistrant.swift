16b0bcf8fd7ebc67ae531c25ffe07b423cfb3726
Tony Wang <yilongwang05@gmail.com>
2025-08-14 15:10:14 -0700

    feat: Update dependencies and configurations
    - Update `flutter_timezone` to version 4.1.1
    - Update other dependencies to their latest versions
    - Ignore `.vscode/` directory
    - Enable core library desugaring for Android
    - Update Android SDK versions
    - Update generated plugin registrant files for Linux, macOS, and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_messaging
 import flutter_local_notifications
 import flutter_timezone
 import google_sign_in_ios
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -27,4 +28,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
