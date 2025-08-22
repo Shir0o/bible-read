c321bc3a723934dd5c78aa06c73c1705acebbf74
Tony Wang <Shir0o@users.noreply.github.com>
2025-08-22 11:58:09 -0700

    Trigger vibration on menu button

--- test/widgets/menu_button_test.dart
+++ test/widgets/menu_button_test.dart
@@ -3,18 +3,29 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bible_read/widgets/app_drawer.dart';
 import 'package:bible_read/widgets/menu_button.dart';
+import 'package:bible_read/services/vibration_service.dart';
+
+class _RecordingVibrationService extends VibrationService {
+  int lightCount = 0;
+
+  @override
+  Future<void> lightImpact() async {
+    lightCount++;
+  }
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('tapping MenuButton opens ancestor drawer', (tester) async {
+    final service = _RecordingVibrationService();
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
           drawer: AppDrawer(onNavigate: (_) {}),
           body: Scaffold(
             appBar: AppBar(
-              leading: const MenuButton(),
+              leading: MenuButton(vibrationService: service),
             ),
           ),
         ),
@@ -26,6 +37,7 @@ void main() {
     await tester.tap(find.byType(MenuButton));
     await tester.pumpAndSettle();
 
+    expect(service.lightCount, 1);
     expect(find.byType(AppDrawer), findsOneWidget);
   });
 }
