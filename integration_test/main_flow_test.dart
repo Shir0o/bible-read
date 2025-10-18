7888de8a364dbf5d2326bb2b29687f825a00cdc4
Tony Wang <yilongwang05@gmail.com>
2025-10-17 17:43:43 -0700

    Remove unused Google Sign-In import from integration test

--- integration_test/main_flow_test.dart
+++ integration_test/main_flow_test.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/src/pigeon/mocks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:integration_test/integration_test.dart';
