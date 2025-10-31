171247eb74270832dcaa32db86ccd77fe3728a2d
Tony Wang <yilongwang05@gmail.com>
2025-10-31 09:30:09 -0700

    test: feedback admin page test

--- test/pages/admin/feedback_admin_page_test.dart
+++ test/pages/admin/feedback_admin_page_test.dart
@@ -6,7 +6,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:matcher/matcher.dart';
 import 'package:mock_exceptions/mock_exceptions.dart';
 
 class _AdminRoleStub extends AdminRoleService {
